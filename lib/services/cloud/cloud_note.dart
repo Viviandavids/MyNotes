3463e00c73793f367791b8bb0787cb3aaf76dace
Vivian <davidvivian201@gmail.com>
2022-08-15 19:00:08 +0100

    step-20

--- lib/services/cloud/cloud_note.dart
+++ lib/services/cloud/cloud_note.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mynotes/services/cloud/cloud_storage_constants.dart';
 
 @immutable
