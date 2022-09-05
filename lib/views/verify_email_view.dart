99d3ae2fad4ca30256c6da425b8d3272375c0c94
Vivian <davidvivian201@gmail.com>
2022-09-05 17:27:06 +0100

    Step-31

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -14,32 +14,36 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Verify Email')),
-      body: Column(
-        children: [
-          const Text(
-            'We have sent you an email verification, please open it to verify your email',
-          ),
-          const Text(
-            "if you havent recieved a verification email yet, press the button below",
-          ),
-          TextButton(
-            onPressed: () async {
-              context.read<AuthBloc>().add(
-                    const AuthEventSendEmailVerification(),
-                  );
-            },
-            child: const Text('Send email verification'),
-          ),
-          TextButton(
-            onPressed: () {
-              context.read<AuthBloc>().add(
-                    const AuthEventLogOut(),
-                  );
-            },
-            child: const Text('Restart'),
-          )
-        ],
+      appBar: AppBar(
+        title: const Text('Verify Email'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Text(
+              'We have sent you an email verification, please open it to verify your email',
+            ),
+            const Text(
+              "if you havent recieved a verification email yet, press the button below",
+            ),
+            TextButton(
+              onPressed: () async {
+                context.read<AuthBloc>().add(
+                      const AuthEventSendEmailVerification(),
+                    );
+              },
+              child: const Text('Send email verification'),
+            ),
+            TextButton(
+              onPressed: () {
+                context.read<AuthBloc>().add(
+                      const AuthEventLogOut(),
+                    );
+              },
+              child: const Text('Restart'),
+            )
+          ],
+        ),
       ),
     );
   }
